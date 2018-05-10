f82278a6150cd7b5bcbc076abf4a6131bdaf0fb4
Justin Kaeser <justin.kaeser@jetbrains.com>
2018-05-10 16:24:50 +0200

    2.0.1

--- Forkit.elm
+++ Forkit.elm
@@ -1,15 +1,10 @@
-module Forkit exposing (harder, better)
+module Forkit exposing (harder)
 {-|
 Fork it harder make it better
 
 @docs harder
-@docs better
 -}
 
 {-| harder! -}
 harder: Int -> Int
 harder n = n+1
-
-{-| better! -}
-better: Int -> Int
-better n = n*100+42
